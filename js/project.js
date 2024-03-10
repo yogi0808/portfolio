0751c2edf9fc208abb8f1cfd8faf408e5fc4b408
yogi0808 <gojiyayogesh08@gmail.com>
2024-03-11 01:38:03 +0530

    added a loader

--- js/project.js
+++ js/project.js
@@ -1,85 +1,94 @@
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+
+    loader.classList.add("loader--hidden");
+
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild(loader);
+    })
+})
+
 gsap.registerPlugin(ScrollTrigger);
 
-document.addEventListener("DOMContentLoaded", () => {
-    let projectData = JSON.parse(localStorage.getItem('singleProject'));
+let projectData = JSON.parse(localStorage.getItem('singleProject'));
 
-    if (!projectData) {
+if (!projectData) {
 
-        projectData = [{
-            id: "1",
-            mimg: "img/luxury photo & frames/6.webp",
-            pname: "Luxury Photo & Frames",
-            ditail: "Designed and developed an online platform dedicated to photography and framing services. Created an intuitive interface allowing Users to explore a wide range of photography options and custom framing solutions. Implemented a user-friendly Gallery showcasing high-quality Images, accompanied by a seamless ordering process for frames tailored to individual preferences. Prioritized user experience by integrating Responsive Design, enabling smooth navigation across devices for an Immersive Visual Experience.",
-            technologies: "PHP, HTML, CSS, JavaScript, MYSQL",
-            imgs: {
-                img1: "img/luxury photo & frames/1.png",
-                img2: "img/luxury photo & frames/2.png",
-                img3: "img/luxury photo & frames/3.png",
-                img4: "img/luxury photo & frames/4.png",
-                img5: "img/luxury photo & frames/5.png",
-            },
-        }];
+    projectData = [{
+        id: "1",
+        mimg: "img/luxury photo & frames/6.webp",
+        pname: "Luxury Photo & Frames",
+        ditail: "Designed and developed an online platform dedicated to photography and framing services. Created an intuitive interface allowing Users to explore a wide range of photography options and custom framing solutions. Implemented a user-friendly Gallery showcasing high-quality Images, accompanied by a seamless ordering process for frames tailored to individual preferences. Prioritized user experience by integrating Responsive Design, enabling smooth navigation across devices for an Immersive Visual Experience.",
+        technologies: "PHP, HTML, CSS, JavaScript, MYSQL",
+        imgs: {
+            img1: "img/luxury photo & frames/1.png",
+            img2: "img/luxury photo & frames/2.png",
+            img3: "img/luxury photo & frames/3.png",
+            img4: "img/luxury photo & frames/4.png",
+            img5: "img/luxury photo & frames/5.png",
+        },
+    }];
 
-    }
-
+}
 
 
 
-    // lenis smooth scroll
-    const lenis = new Lenis({
-        duration: 1.2,
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
-    });
 
-    function raf(time) {
-        lenis.raf(time);
-        ScrollTrigger.update();
-        requestAnimationFrame(raf);
-    }
+// lenis smooth scroll
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+});
 
+function raf(time) {
+    lenis.raf(time);
+    ScrollTrigger.update();
     requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 
 
-    const mouse = document.querySelector('.mouse-circul');
-    const links = document.querySelectorAll('.nav-links li')
-    const wraper = document.querySelector('.wraper');
-    const menuBtn = document.querySelector(".menu-btn");
-    const menu = document.querySelector(".nav-links");
+const mouse = document.querySelector('.mouse-circul');
+const links = document.querySelectorAll('.nav-links li')
+const wraper = document.querySelector('.wraper');
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".nav-links");
 
-    let html = "";
-    let iData = "";
-    let link = "";
-    let selectedImage = projectData.imgs[0];
+let html = "";
+let iData = "";
+let link = "";
+let selectedImage = projectData.imgs[0];
 
 
-    // for nav menu btn
-    menuBtn.addEventListener("click", () => {
-        const currentState = menuBtn.getAttribute("data-state");
+// for nav menu btn
+menuBtn.addEventListener("click", () => {
+    const currentState = menuBtn.getAttribute("data-state");
 
-        if (!currentState || currentState === "closed") {
-            menuBtn.setAttribute("data-state", "opened");
-            menuBtn.setAttribute("aria-expanded", "true");
-        } else {
-            menuBtn.setAttribute("data-state", "closed");
-            menuBtn.setAttribute("aria-expanded", "false");
-        }
+    if (!currentState || currentState === "closed") {
+        menuBtn.setAttribute("data-state", "opened");
+        menuBtn.setAttribute("aria-expanded", "true");
+    } else {
+        menuBtn.setAttribute("data-state", "closed");
+        menuBtn.setAttribute("aria-expanded", "false");
+    }
 
-        menu.classList.toggle("active")
+    menu.classList.toggle("active")
 
-    });
+});
 
-    function generateHTML() {
+function generateHTML() {
 
-        console.log(selectedImage)
+    console.log(selectedImage)
 
-        projectData.imgs.forEach(img => {
+    projectData.imgs.forEach(img => {
 
-            iData += `
+        iData += `
             <div class="img"><img class="small-img" src="${img}" alt=""></div>
             `
-        })
+    })
 
-        html = `
+    html = `
                 <section class="ditail">
                 <div class="text">
                     <h1>${projectData.pname}</h1>
@@ -103,110 +112,109 @@ document.addEventListener("DOMContentLoaded", () => {
             </div>
                 `
 
-        return html
+    return html
+
+}
+
+generateHTML()
+
+wraper.innerHTML = html;
+
+
+
+
+// mouse circul
+let mouseX = -200
+let mouseY = -200
+
+window.addEventListener("mousemove", (e) => {
+
+    mouseX = event.clientX
+    mouseY = event.clientY
+
+    window.requestAnimationFrame(mouseMove);
+})
+
+const mouseMove = () => {
+
+    if (window.innerWidth > 770) {
+
+        mouse.style.left = mouseX - 9 + "px"
+        mouse.style.top = mouseY - 5 + "px"
+
+    } else {
 
     }
 
-    generateHTML()
 
-    wraper.innerHTML = html;
+}
 
+mouseMove()
 
-
-
-    // mouse circul
-    let mouseX = -200
-    let mouseY = -200
-
-    window.addEventListener("mousemove", (e) => {
-
-        mouseX = event.clientX
-        mouseY = event.clientY
-
-        window.requestAnimationFrame(mouseMove);
+// nav scale effect
+links.forEach(link => {
+    link.addEventListener("mouseenter", () => {
+        mouse.style.scale = 2
     })
-
-    const mouseMove = () => {
-
-        if (window.innerWidth > 770) {
-
-            mouse.style.left = mouseX - 9 + "px"
-            mouse.style.top = mouseY - 5 + "px"
-
-        } else {
-
-        }
-
-
-    }
-
-    mouseMove()
-
-    // nav scale effect
-    links.forEach(link => {
-        link.addEventListener("mouseenter", () => {
-            mouse.style.scale = 2
-        })
-        link.addEventListener("mouseleave", () => {
-            mouse.style.scale = 1
-        })
-    })
-
-    gsap.from(".logo", {
-        opacity: 0,
-        y: "20px",
-        duration: 0.7,
-        ease: Power2,
-    })
-
-    gsap.from(links, {
-        stagger: .2,
-        opacity: 0,
-        y: "20px",
-        duration: 0.7,
-    })
-
-    gsap.from(".text h1", {
-        scale: 0,
-        duration: 1,
-    })
-
-    gsap.from(".text p", {
-        scale: 0,
-        duration: 1,
-    })
-
-    gsap.from(".text2 .technologies", {
-        scale: 0,
-        duration: 1,
-    })
-
-    gsap.from(".img-list", {
-        scale: 0,
-        duration: 1,
-    })
-
-
-
-
-
-    // logo scale effect
-    document.querySelector(".logo").addEventListener("mouseenter", () => {
-        mouse.style.scale = 3
-    })
-    document.querySelector(".logo").addEventListener("mouseleave", () => {
+    link.addEventListener("mouseleave", () => {
         mouse.style.scale = 1
     })
+})
+
+gsap.from(".logo", {
+    opacity: 0,
+    y: "20px",
+    duration: 0.7,
+    ease: Power2,
+})
+
+gsap.from(links, {
+    stagger: .2,
+    opacity: 0,
+    y: "20px",
+    duration: 0.7,
+})
+
+gsap.from(".text h1", {
+    scale: 0,
+    duration: 1,
+})
+
+gsap.from(".text p", {
+    scale: 0,
+    duration: 1,
+})
+
+gsap.from(".text2 .technologies", {
+    scale: 0,
+    duration: 1,
+})
+
+gsap.from(".img-list", {
+    scale: 0,
+    duration: 1,
+})
 
 
-    // for photo chenge onclick
-    var allImage = document.querySelectorAll(".small-img");
 
-    allImage.forEach(img => {
 
-        img.addEventListener("click", () => {
-            document.querySelector(".main-img").src = img.src
-        })
 
+// logo scale effect
+document.querySelector(".logo").addEventListener("mouseenter", () => {
+    mouse.style.scale = 3
+})
+document.querySelector(".logo").addEventListener("mouseleave", () => {
+    mouse.style.scale = 1
+})
+
+
+// for photo chenge onclick
+var allImage = document.querySelectorAll(".small-img");
+
+allImage.forEach(img => {
+
+    img.addEventListener("click", () => {
+        document.querySelector(".main-img").src = img.src
     })
-});
+
+})
--- js/script.js
+++ js/script.js
@@ -2,339 +2,343 @@ import { works } from "../data/works.js";
 
 export let singleProject;
 
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+
+    loader.classList.add("loader--hidden");
+
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild(loader);
+    })
+})
+
 function saveToStorage() {
     localStorage.setItem('singleProject', JSON.stringify(singleProject))
 }
 
 gsap.registerPlugin(ScrollTrigger);
-document.addEventListener("DOMContentLoaded", () => {
 
-    // lenis smooth scroll
-    const lenis = new Lenis({
-        duration: 1.2,
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
-    });
 
-    function raf(time) {
-        lenis.raf(time);
-        ScrollTrigger.update();
-        requestAnimationFrame(raf);
-    }
+// lenis smooth scroll
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+});
 
+function raf(time) {
+    lenis.raf(time);
+    ScrollTrigger.update();
     requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 
 
-    const mouse = document.querySelector('.mouse-circul');
-    const logo = document.querySelector(".logo");
-    const links = document.querySelectorAll("ul li a");
-    const menuBtn = document.querySelector(".menu-btn");
-    const menu = document.querySelector(".nav-links");
-    const svgs = document.querySelectorAll(".svg");
-    const pro = document.querySelector(".pro");
+const mouse = document.querySelector('.mouse-circul');
+const logo = document.querySelector(".logo");
+const links = document.querySelectorAll("ul li a");
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".nav-links");
+const svgs = document.querySelectorAll(".svg");
+const pro = document.querySelector(".pro");
 
-    // mouse circul
-    let mouseX = -200
-    let mouseY = -200
+// mouse circul
+let mouseX = -200
+let mouseY = -200
 
-    window.addEventListener("mousemove", (e) => {
+window.addEventListener("mousemove", (e) => {
 
-        mouseX = event.clientX
-        mouseY = event.clientY
+    mouseX = event.clientX
+    mouseY = event.clientY
 
-        window.requestAnimationFrame(mouseMove);
-    })
+    window.requestAnimationFrame(mouseMove);
+})
 
-    const mouseMove = () => {
+const mouseMove = () => {
 
-        if (window.innerWidth > 770) {
+    if (window.innerWidth > 770) {
 
-            mouse.style.left = mouseX - 9 + "px"
-            mouse.style.top = mouseY - 5 + "px"
+        mouse.style.left = mouseX - 9 + "px"
+        mouse.style.top = mouseY - 5 + "px"
 
-        } else {
-
-        }
-
+    } else {
 
     }
 
-    mouseMove()
+
+}
+
+mouseMove()
 
 
-    let proHTML = "";
+let proHTML = "";
 
-    renderProject()
+renderProject()
 
-    pro.innerHTML = proHTML;
+pro.innerHTML = proHTML;
 
-    function renderProject() {
-        works.forEach(work => {
-            proHTML += `
+function renderProject() {
+    works.forEach(work => {
+        proHTML += `
             <div class="project ${work.class}" data-id="${work.id}">
                 <img src="${work.mimg}" alt="project">
                 <h3>${work.pname}</h3>
             </div>
             `
 
-            return proHTML
-        })
+        return proHTML
+    })
+}
+
+// onclick=""
+
+// nav link circle scale
+links.forEach(link => {
+    link.addEventListener("mouseenter", () => {
+        mouse.style.scale = 2
+    })
+    link.addEventListener("mouseleave", () => {
+        mouse.style.scale = 1
+    })
+})
+
+
+
+// for nav menu btn
+menuBtn.addEventListener("click", () => {
+    const currentState = menuBtn.getAttribute("data-state");
+
+    if (!currentState || currentState === "closed") {
+        menuBtn.setAttribute("data-state", "opened");
+        menuBtn.setAttribute("aria-expanded", "true");
+    } else {
+        menuBtn.setAttribute("data-state", "closed");
+        menuBtn.setAttribute("aria-expanded", "false");
     }
 
-    // onclick=""
+    menu.classList.toggle("active")
 
-    // nav link circle scale
-    links.forEach(link => {
-        link.addEventListener("mouseenter", () => {
-            mouse.style.scale = 2
-        })
-        link.addEventListener("mouseleave", () => {
-            mouse.style.scale = 1
-        })
+    logo.style.zIndex = logo.style.zIndex === "8" ? "10" : "8";
+
+});
+
+
+
+
+// logo circle scale
+logo.addEventListener("mouseenter", () => {
+    mouse.style.scale = 4
+})
+logo.addEventListener("mouseleave", () => {
+    mouse.style.scale = 1
+})
+
+
+// hero img circle scale
+document.querySelector(".hero-img img").addEventListener("mouseenter", () => {
+    mouse.style.scale = 7
+})
+document.querySelector(".hero-img img").addEventListener("mouseleave", () => {
+    mouse.style.scale = 1
+})
+
+
+// project circle scale and arrow show 
+document.querySelectorAll(".project").forEach(pro => {
+
+    pro.addEventListener("mouseenter", () => {
+        mouse.style.scale = 4
+        mouse.classList.add('arrow')
+
+    })
+    pro.addEventListener("mouseleave", () => {
+        mouse.style.scale = 1
+        mouse.classList.remove('arrow')
     })
 
+    pro.addEventListener("click", () => {
 
+        console.log(pro.dataset.id)
 
-    // for nav menu btn
-    menuBtn.addEventListener("click", () => {
-        const currentState = menuBtn.getAttribute("data-state");
+        works.forEach(work => {
+            if (pro.dataset.id === work.id) {
+                singleProject = work;
+                console.log(singleProject)
+            }
+        })
 
-        if (!currentState || currentState === "closed") {
-            menuBtn.setAttribute("data-state", "opened");
-            menuBtn.setAttribute("aria-expanded", "true");
-        } else {
-            menuBtn.setAttribute("data-state", "closed");
-            menuBtn.setAttribute("aria-expanded", "false");
-        }
+        saveToStorage();
+        window.location.href = 'project.html';
+    })
 
-        menu.classList.toggle("active")
-
-        logo.style.zIndex = logo.style.zIndex === "8" ? "10" : "8";
-
-    });
+})
 
 
 
+document.getElementById("submit-btn").addEventListener("click", () => {
+    sendMessage();
 
-    // logo circle scale
-    logo.addEventListener("mouseenter", () => {
+})
+
+function sendMessage() {
+
+    (function () {
+        emailjs.init("a7gNHaSCLxUg4q4oT");
+    })();
+
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_rapdf4q";
+    const templateID = "template_uxye17i";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("subject").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Thakn you, " + params["name"] + "! Your message has been sent.")
+
+        })
+        .catch(err => console.log(err));
+}
+
+
+
+// about img circle blend mode chenge
+document.querySelector("#about .img img").addEventListener("mouseenter", () => {
+    mouse.style.mixBlendMode = 'normal'
+})
+document.querySelector("#about .img img").addEventListener("mouseleave", () => {
+    mouse.style.mixBlendMode = 'difference'
+})
+
+
+
+// svg circle scale
+svgs.forEach(svg => {
+    svg.addEventListener("mouseenter", () => {
         mouse.style.scale = 4
     })
-    logo.addEventListener("mouseleave", () => {
+    svg.addEventListener("mouseleave", () => {
         mouse.style.scale = 1
     })
+})
 
 
-    // hero img circle scale
-    document.querySelector(".hero-img img").addEventListener("mouseenter", () => {
-        mouse.style.scale = 7
-    })
-    document.querySelector(".hero-img img").addEventListener("mouseleave", () => {
-        mouse.style.scale = 1
-    })
+// gsap timeline for logo
+let tl = gsap.timeline({
 
-
-    // project circle scale and arrow show 
-    document.querySelectorAll(".project").forEach(pro => {
-
-        pro.addEventListener("mouseenter", () => {
-            mouse.style.scale = 4
-            mouse.classList.add('arrow')
-
-        })
-        pro.addEventListener("mouseleave", () => {
-            mouse.style.scale = 1
-            mouse.classList.remove('arrow')
-        })
-
-        pro.addEventListener("click", () => {
-
-            console.log(pro.dataset.id)
-
-            works.forEach(work => {
-                if (pro.dataset.id === work.id) {
-                    singleProject = work;
-                    console.log(singleProject)
-                }
-            })
-
-            saveToStorage();
-            window.location.href = 'project.html';
-        })
-
-    })
-
-
-
-    document.getElementById("submit-btn").addEventListener("click", () => {
-        sendMessage();
-
-    })
-
-    function sendMessage() {
-
-        (function () {
-            emailjs.init("a7gNHaSCLxUg4q4oT");
-        })();
-
-        var params = {
-            name: document.getElementById("name").value,
-            email: document.getElementById("email").value,
-            subject: document.getElementById("subject").value,
-            message: document.getElementById("message").value,
-        };
-
-        const serviceID = "service_rapdf4q";
-        const templateID = "template_uxye17i";
-
-        emailjs.send(serviceID, templateID, params)
-            .then(res => {
-                document.getElementById("name").value = "";
-                document.getElementById("email").value = "";
-                document.getElementById("subject").value = "";
-                document.getElementById("message").value = "";
-                console.log(res);
-                alert("Thakn you, " + params["name"] + "! Your message has been sent.")
-
-            })
-            .catch(err => console.log(err));
+    scrollTrigger: {
+        trigger: "#home",
+        pin: true,
+        start: "top top",
+        end: "77% top",
+        scrub: 2,
+        // markers: true,
     }
+})
+
+
+// gsap animation for logo
+
+tl.to(logo, {
+    ease: true,
+    scale: 1,
+    top: 10 + 'px',
+    left: 64 + 'px',
+    x: 0,
+    y: 0,
+    color: "#dadada",
+})
 
 
 
-    // about img circle blend mode chenge
-    document.querySelector("#about .img img").addEventListener("mouseenter", () => {
-        mouse.style.mixBlendMode = 'normal'
-    })
-    document.querySelector("#about .img img").addEventListener("mouseleave", () => {
-        mouse.style.mixBlendMode = 'difference'
-    })
+// gsap animation for nav links
+gsap.from("li", {
+    stagger: .2,
+    opacity: 0,
+    y: "20px",
+    duration: 0.7,
+    ease: Power2,
+})
+
+// gsap animation on svgs 
+gsap.to(svgs, {
+    scale: 1,
+    y: 0,
+    scrollTrigger: {
+        trigger: ".svgs",
+        start: "-40% 85%",
+        end: "bottom 85%",
+        scrub: 2,
+        // markers: true,
+    }
+})
 
 
+// gsap animation for hero text
+gsap.from(".hero-text", {
+    stagger: .2,
+    opacity: 0,
+    y: "40px",
+    duration: 1,
+    ease: Power2,
+})
 
-    // svg circle scale
-    svgs.forEach(svg => {
-        svg.addEventListener("mouseenter", () => {
-            mouse.style.scale = 4
-        })
-        svg.addEventListener("mouseleave", () => {
-            mouse.style.scale = 1
-        })
-    })
+// gsap animation for hero img
+gsap.from(".hero-img img", {
+    scale: -1,
+    rotate: -360,
+    duration: 1,
+    ease: Power2,
+})
 
+// gsap animation for about img
+gsap.to("#about .img img", {
+    scale: 1,
+    y: 0,
+    scrollTrigger: {
+        trigger: "#about .img",
+        start: "top 85%",
+        end: "70% bottom",
+        // markers: true,
+        scrub: 2,
+    }
+})
 
-    // gsap timeline for logo
-    let tl = gsap.timeline({
+// gsap animation for about texts
+gsap.to("#about .info p", {
+    lineHeight: 1.2,
+    scrollTrigger: {
+        trigger: "#about .info",
+        start: "top 80%",
+        end: "70% bottom",
+        // markers: true,
+        scrub: 2,
+    }
+})
 
-        scrollTrigger: {
-            trigger: "#home",
-            pin: true,
-            start: "top top",
-            end: "77% top",
-            scrub: 2,
-            // markers: true,
-        }
-    })
+let pros = gsap.utils.toArray(".project");
 
-
-    // gsap animation for logo
-
-    tl.to(logo, {
-        ease: true,
-        scale: 1,
-        top: 10 + 'px',
-        left: 64 + 'px',
-        x: 0,
-        y: 0,
-        color: "#dadada",
-    })
-
-
-
-    // gsap animation for nav links
-    gsap.from("li", {
-        stagger: .2,
-        opacity: 0,
-        y: "20px",
-        duration: 0.7,
-        ease: Power2,
-    })
-
-    // gsap animation on svgs 
-    gsap.to(svgs, {
-        scale: 1,
-        y: 0,
-        scrollTrigger: {
-            trigger: ".svgs",
-            start: "-40% 85%",
-            end: "bottom 85%",
-            scrub: 2,
-            // markers: true,
-        }
-    })
-
-
-    // gsap animation for hero text
-    gsap.from(".hero-text", {
-        stagger: .2,
-        opacity: 0,
-        y: "40px",
-        duration: 1,
-        ease: Power2,
-    })
-
-    // gsap animation for hero img
-    gsap.from(".hero-img img", {
-        scale: -1,
-        rotate: -360,
-        duration: 1,
-        ease: Power2,
-    })
-
-    // gsap animation for about img
-    gsap.to("#about .img img", {
-        scale: 1,
-        y: 0,
-        scrollTrigger: {
-            trigger: "#about .img",
-            start: "top 85%",
-            end: "70% bottom",
-            // markers: true,
-            scrub: 2,
-        }
-    })
-
-    // gsap animation for about texts
-    gsap.to("#about .info p", {
-        lineHeight: 1.2,
-        scrollTrigger: {
-            trigger: "#about .info",
-            start: "top 80%",
-            end: "70% bottom",
-            // markers: true,
-            scrub: 2,
-        }
-    })
-
-    let pros = gsap.utils.toArray(".project");
-
-    gsap.to(pros, {
-        opacity: 1,
-        y: 0,
-        ease: "none",
-        scale: 1,
-        scrollTrigger: {
-            trigger: ".project",
-            scrub: 2,
-            start: "-40% 85%",
-            end: "40% bottom",
-            scrub: 2,
-            // markers: true,
-        }
-
-    })
-
-
-
-
+gsap.to(pros, {
+    opacity: 1,
+    y: 0,
+    ease: "none",
+    scale: 1,
+    scrollTrigger: {
+        trigger: ".project",
+        scrub: 2,
+        start: "-40% 85%",
+        end: "40% bottom",
+        scrub: 2,
+        // markers: true,
+    }
 
 })
